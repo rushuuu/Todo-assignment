466ce1464baf7849ef295824de58b7c54768b401
rushuuu <rushilp4589@gmail.com>
2025-01-07 23:13:33 +0530

    File and Folder Structure created for Assignment

---
+++ src/todo_manager.py
@@ -0,0 +1,14 @@
+import uuid
+from datetime import datetime
+
+# Create a class Task with constructor id description duedate completed
+
+class Task:
+    def __init__(self, description: str, due_date: str):
+        self.id = str(uuid.uuid4())
+        self.description = description
+        self.due_date = due_date
+        self.completed = False
+
+    def __repr__(self):
+        return f"Task(id={self.id}, description={self.description}, due_date={self.due_date}, completed={self.completed})"
